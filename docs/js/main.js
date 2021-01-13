8df401978cf658fffe63a26e2997d3428df07254
Naiara Abaroa <naiaraabaroa@gmail.com>
2021-01-13 21:53:08 +0100

    Add graph

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,70 @@
 // Lekeitio Covid graph
+
+// Health Tracking Graph
+
+// set the dimensions and margins of the graph
+var margin = { top: 10, right: 30, bottom: 180, left: 60 },
+  width = 900 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3
+  .select("#graph")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+//Read the data
+d3.csv(
+  "https://raw.githubusercontent.com/nabaroa/lekeitio-covid/main/docs/data/data.csv",
+
+  // When reading the csv, I must format variables:
+  function (d) {
+    return {
+      date: d3.timeParse("%Y-%m-%d")(d.date),
+      positives: d.positives,
+    };
+  },
+
+  // Now I can use this dataset:
+  function (data) {
+    // Add X axis --> it is a date format
+    var x = d3
+      .scaleTime()
+      .domain(
+        d3.extent(data, function (d) {
+          return d.date;
+        })
+      )
+      .range([0, width]);
+    svg
+      .append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y = d3.scaleLinear().domain([0, 10]).range([height, 0]);
+    svg.append("g").call(d3.axisLeft(y));
+
+    // Add the positives line
+    svg
+      .append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "pink")
+      .attr("stroke-width", 1.5)
+      .attr(
+        "d",
+        d3
+          .line()
+          .x(function (d) {
+            return x(d.date);
+          })
+          .y(function (d) {
+            return y(d.positives);
+          })
+      );
+  }
+);
